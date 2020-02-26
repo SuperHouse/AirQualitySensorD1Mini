10b576e602918e3a1f54c634a4ed2db3ad0ab0d8
Jonathan Oxer <jon@oxer.com.au>
2020-02-26 23:12:20 +1100

    Changed report interval back to 2 minutes

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ const char* password = "YOUR PSK";           // Your WiFi password
 /* MQTT */
 const char* mqtt_broker  = "192.168.1.111";  // IP address of your MQTT broker
 const char* statusTopic  = "events";         // MQTT topic to report startup
-uint32_t report_interval = 10;              // Report interval in seconds
+uint32_t report_interval = 120;              // Report interval in seconds
 
 /* ----------------- Hardware-specific Config ---------------------- */
 /* Mode button (momentary between this pin and GND) */
